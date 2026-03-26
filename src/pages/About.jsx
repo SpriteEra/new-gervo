e8d8f8563607e8aad0e55f68c5f17485d88cf033
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2026-03-27 01:16:39 +0530

    full added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import AboutHero from '../components/AboutTop'
 import Whoweare from '../components/Whoweare'
+import StatsSection from '../common/StatsSection'
+import ChooseUs from '../components/ChooseUs'
+import OurServices from '../components/OurServices'
+import ProcessAndTestimonials from '../components/ProcessAndTestimonials'
+import InnovationSection from '../components/InnovationSection'
 const About = () => {
     return (
         <div>
             <AboutHero />
             <Whoweare/>
+            <StatsSection/>
+            <ChooseUs/>
+            <OurServices/>
+            <ProcessAndTestimonials/>
+            <InnovationSection/>
         </div>
     )
 }
