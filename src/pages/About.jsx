75bd4053553c63b0f02eb2da68065cc7e82b8ed0
Ashutosh1700 <94374532+Ashutosh1700@users.noreply.github.com>
2026-03-28 18:03:30 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,16 +6,20 @@ import ChooseUs from '../components/ChooseUs'
 import OurServices from '../components/OurServices'
 import ProcessAndTestimonials from '../components/ProcessAndTestimonials'
 import InnovationSection from '../components/InnovationSection'
+import Footer1 from '../components/Footer1'
+import TextDesign from '../components/TextDesign'
 const About = () => {
     return (
         <div>
             <AboutHero />
-            <Whoweare/>
-            <StatsSection/>
-            <ChooseUs/>
-            <OurServices/>
-            <ProcessAndTestimonials/>
-            <InnovationSection/>
+            <Whoweare />
+            <StatsSection />
+            <ChooseUs />
+            <OurServices />
+            <ProcessAndTestimonials />
+            <InnovationSection />
+            <Footer1 />
+            <TextDesign />
         </div>
     )
 }
